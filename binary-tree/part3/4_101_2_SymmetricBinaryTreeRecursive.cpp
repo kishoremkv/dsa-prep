f8028869bb5bf015b0dd2376934196652929a6b0
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2024-08-18 14:19:54 +0530

    Create 4_101_2_SymmetricBinaryTreeRecursive.cpp

---
+++ binary-tree/part3/4_101_2_SymmetricBinaryTreeRecursive.cpp
@@ -0,0 +1,26 @@
+// Recursive approach
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool check(TreeNode* left, TreeNode* right)
+    {
+        if(left == NULL && right == NULL) return true;
+        if(left == NULL || right == NULL) return false;
+
+        return (left->val == right->val) && check(left->left, right->right) && check(left->right, right->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left, root->right);
+    }
+};
